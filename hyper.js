c1e076825697523b51e94c2ab1b154e96710ae2b
mph <mph@mph.info>
2019-03-14 15:32:42 -0700

    Change Hyper shell command to zsh

--- hyper.js
+++ hyper.js
@@ -69,12 +69,18 @@ module.exports = {
     // default font size in pixels for all tabs
     fontSize: 16,
 
+    // default font weight
+    fontWeight: 'normal',
+
+    // default font weight for bold characters
+    fontWeightBold: 'normal',
+
     // custom padding (css format, i.e.: `top right bottom left`)
     padding: '12px 14px',
 
     // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
     // if left empty, your system's login shell will be used by default
-    shell: '/usr/local/bin/bash',
+    shell: '/usr/local/bin/zsh',
 
     // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
     // by default ['--login'] will be used
